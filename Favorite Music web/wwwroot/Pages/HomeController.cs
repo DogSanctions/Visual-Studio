ef2d131106b9b41402e595ac3770ad48c9d13f42
Jared Hubbard <hubbard.abstract@gmail.com>
2020-05-06 22:01:01 -0700

    Favorite Music Website
    After spending two days scouring the web and joining 12 different discord coding groups I haven't yet found an answer to my questions.  Do I need to link my static pages in ASP .NET CORE to my index.cshtml or is it that the order in which they are all placed in the "pipeline" effects whether or not they will load?

---
+++ Favorite Music web/wwwroot/Pages/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace Favorite_Music_web.Pages
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Favorite Music web/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+     function myMove() {
+  var elem = document.getElementById("animate");
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+        clearInterval(id);
+    } else {
+        pos++;
+      elem.style.top = pos + "px";
+      elem.style.left = pos + "px";
+    }
+  }
+}
